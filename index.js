4878cf3e1f1cacac01b2d40ea1e70a570868cb37
Kevin CORRE <kevin1.corre@orange.com>
2016-10-04 14:02:59 +0200

    Modified version for RS256 and JWT with JWK in header

--- index.js
+++ index.js
@@ -18,22 +18,26 @@ extend = require('extend'),
 url = require('url'),
 Q = require('q'),
 jwt = require('jwt-simple'),
+pem2jwk = require('pem-jwk').pem2jwk,
 util = require("util"),
 base64url = require('base64url'),
 cleanObj = require('clean-obj');
 
 
+
+var ursa = require('ursa');
+
 var defaults = {
         login_url: '/login',
         consent_url: '/consent',
-        iss: null,
         scopes: {
             openid: 'Informs the Authorization Server that the Client is making an OpenID Connect request.',
             profile:'Access to the End-User\'s default profile Claims.',
             email: 'Access to the email and email_verified Claims.',
             address: 'Access to the address Claim.',
             phone: 'Access to the phone_number and phone_number_verified Claims.',
-            offline_access: 'Grants access to the End-User\'s UserInfo Endpoint even when the End-User is not present (not logged in).'
+            offline_access: 'Grants access to the End-User\'s UserInfo Endpoint even when the End-User is not present (not logged in).',
+            webrtc: 'Grants access to basic user-information for WebRTC Identity communication.'
         },
         policies:{
             loggedIn: function(req, res, next) {
@@ -106,28 +110,34 @@ var defaults = {
                     schema: true,
                     policies: 'loggedIn',
                     attributes: {
+                        required_sig: {type: 'string', defaultsTo: 'HS256'},
                         key: {type: 'string', required: true, unique: true},
                         secret: {type: 'string', required: true, unique: true},
                         name: {type: 'string', required: true},
                         image: 'binary',
-                        user: {model: 'user'},
+                        owner: {model: 'user'},
                         redirect_uris: {type:'array', required: true},
                         credentialsFlow: {type: 'boolean', defaultsTo: false}
                     },
                     beforeCreate: function(values, next) {
-                        if(!values.key) {
-                            var sha256 = crypto.createHash('sha256');
-                            sha256.update(values.name);
-                            sha256.update(Math.random()+'');
-                            values.key = sha256.digest('hex');
-                        }
-                        if(!values.secret) {
-                            var sha256 = crypto.createHash('sha256');
-                            sha256.update(values.key);
-                            sha256.update(values.name);
-                            sha256.update(Math.random()+'');
-                            values.secret = sha256.digest('hex');
-                        }
+                         switch(values.required_sig) {
+                             case 'RS256':
+                                  var key = ursa.generatePrivateKey(1024, 65537);
+                                  values.secret = key.toPrivatePem('base64');
+                                  values.key = key.toPublicPem('base64');
+                                  break;
+                             default:
+                                  values.required_sig = 'HS256'
+                                  values.key = crypto.createHash('sha256')
+                                        .update(values.name)
+                                        .update(Math.random()+'')
+                                        .digest('hex');
+                                  values.secret = crypto.createHash('sha256')
+                                        .update(values.key)
+                                        .update(values.name)
+                                        .update(Math.random()+'')
+                                        .digest('hex');
+                         }
                         next();
                     }
                 },
@@ -311,6 +321,9 @@ OpenIDConnect.prototype.errorHandle = function(res, uri, error, desc) {
         var redirect = url.parse(uri,true);
         redirect.query.error = error; //'invalid_request';
         redirect.query.error_description = desc; //'Parameter '+x+' is mandatory.';
+        console.log(error)
+        console.log(desc)
+        console.log(uri)
         res.redirect(400, url.format(redirect));
     } else {
         res.send(400, error+': '+desc);
@@ -437,6 +450,7 @@ OpenIDConnect.prototype.auth = function() {
             nonce: function(params){
                 return params.response_type.indexOf('id_token')!==-1;
             },
+            rtcsdp: false,//function(params){ return params.scope.indexOf('webrtc')!==-1},
             display: false,
             prompt: false,
             max_age: false,
@@ -477,6 +491,8 @@ OpenIDConnect.prototype.auth = function() {
                         } else {
                             req.session.client_id = model.id;
                             req.session.client_secret = model.secret;
+                            //Find required signature by client
+                            req.session.required_sig = model.required_sig
                             deferred.resolve(params);
                         }
                     });
@@ -580,15 +596,16 @@ OpenIDConnect.prototype.auth = function() {
                                 break;
                             case 'id_token':
                                 var d = Math.round(new Date().getTime()/1000);
-                                //var id_token = {
-                                def.resolve({id_token: {
-                                        iss: self.settings.iss||req.protocol+'://'+req.headers.host,
+                                var id_token= {
+                                        iss: req.protocol+'://'+req.headers.host,
                                         sub: req.session.sub||req.session.user,
                                         aud: params.client_id,
                                         exp: d+3600,
                                         iat: d,
-                                        nonce: params.nonce
-                                }});
+                                        nonce: params.nonce,
+                                }
+                                if(params.rtcsdp) id_token.rtcsdp = params.rtcsdp
+                                def.resolve({id_token: id_token});
                                 //def.resolve({id_token: jwt.encode(id_token, req.session.client_secret)});
                                 break;
                             case 'token':
@@ -638,7 +655,17 @@ OpenIDConnect.prototype.auth = function() {
                             if(resp.access_token && resp.id_token) {
                                 var hbuf = crypto.createHmac('sha256', req.session.client_secret).update(resp.access_token).digest();
                                 resp.id_token.at_hash = base64url(hbuf.toString('ascii', 0, hbuf.length/2));
-                                resp.id_token = jwt.encode(resp.id_token, req.session.client_secret);
+                                //resp.id_token = jwt.encode(resp.id_token, new Buffer(req.session.client_secret, 'base64').toString('binary'), "HS256");
+
+                                var key = new Buffer(req.session.client_key, 'base64').toString("ascii")
+                                var jwk = pem2jwk(key)
+                                resp.id_token = jwt.encode(resp.id_token,
+                                                           req.session.required_sig == "RS256" ?
+                                                                new Buffer(req.session.client_secret, 'base64').toString('binary') :
+                                                                req.session.client_secret,
+                                                           req.session.required_sig,
+                                                           {header:{jwk:jwk}});
+                                console.log('done')
                             }
                             deferred.resolve({params: params, type: params.response_type != 'code'?'f':'q', resp: resp});
                         });
@@ -659,7 +686,16 @@ OpenIDConnect.prototype.auth = function() {
                         } else {
                             uri.query = resp;
                         }
-                        res.redirect(url.format(uri));
+                        //This is an actual request for an ID Assertion
+                        if(params.rtcsdp){
+                         console.log('SEND')
+                         res.send(uri.hash)
+                        }
+                        //Else this is a standard authorization request
+                        else {
+                            console.log('RDR'+url.format(uri))
+                            res.redirect(url.format(uri));
+                        }
                     }
                 })
                 .fail(function(error) {
@@ -780,10 +816,10 @@ OpenIDConnect.prototype.token = function() {
                         .exec(function(err, auth) {
                             if(!err && auth) {
                                 if(auth.status != 'created') {
-                                    auth.refreshTokens.forEach(function(refresh) {
+                                    auth.refresh.forEach(function(refresh) {
                                         refresh.destroy();
                                     });
-                                    auth.accessTokens.forEach(function(access) {
+                                    auth.access.forEach(function(access) {
                                         access.destroy();
                                     });
                                     auth.destroy();
@@ -827,10 +863,10 @@ OpenIDConnect.prototype.token = function() {
                                 .populate('client')
                                 .exec(function(err, auth) {
                                     if(refresh.status != 'created') {
-                                        auth.accessTokens.forEach(function(access){
+                                        auth.access.forEach(function(access){
                                             access.destroy();
                                         });
-                                        auth.refreshTokens.forEach(function(refresh){
+                                        auth.refresh.forEach(function(refresh){
                                             refresh.destroy();
                                         });
                                         auth.destroy();
@@ -913,7 +949,7 @@ OpenIDConnect.prototype.token = function() {
 		                            .populate('accessTokens')
 		                            .populate('refreshTokens')
                                     .exec(function(err, auth) {
-                                        if(auth && !auth.accessTokens.length && !auth.refreshTokens.length) {
+                                        if(auth && !auth.access.length && !auth.refresh.length) {
                                             auth.destroy();
                                         }
                                     });
@@ -922,19 +958,25 @@ OpenIDConnect.prototype.token = function() {
 
                             var d = Math.round(new Date().getTime()/1000);
                             var id_token = {
-                                    iss: self.settings.iss||req.protocol+'://'+req.headers.host,
+                                    iss: req.protocol+'://'+req.headers.host,
                                     sub: prev.sub||prev.user||null,
                                     aud: prev.client.key,
                                     exp: d+3600,
                                     iat: d
                             };
+                            var jwk = pem2jwk(new Buffer(prev.client.key, 'base64').toString('ascii'))
+                            console.log('encoding with header ... ')
                             req.model.access.create({
                                     token: access,
                                     type: 'Bearer',
                                     expiresIn: 3600,
                                     user: prev.user||null,
                                     client: prev.client.id,
-                                    idToken: jwt.encode(id_token, prev.client.secret),
+                                    idToken: jwt.encode(id_token,
+                                                        prev.client.required_sig == "RS256" ? new Buffer(prev.client.secret, 'base64').toString('binary') :
+                                                                                                       prev.client.secret,
+                                                        prev.client.required_sig,
+                                                        {header:{jwk:jwk}}),
                                     scope: prev.scope,
                                     auth: prev.auth?prev.auth.id:null
                             },
@@ -952,7 +994,7 @@ OpenIDConnect.prototype.token = function() {
 				                            .populate('accessTokens')
 				                            .populate('refreshTokens')
                                             .exec(function(err, auth) {
-                                                if(auth && !auth.accessTokens.length && !auth.refreshTokens.length) {
+                                                if(auth && !auth.access.length && !auth.refresh.length) {
                                                     auth.destroy();
                                                 }
                                             });
@@ -1048,7 +1090,7 @@ OpenIDConnect.prototype.check = function() {
                                 });
                                 if(errors.length > 1) {
                                     var last = errors.pop();
-                                    self.errorHandle(res, null, 'invalid_scope', 'Required scopes '+errors.join(', ')+' and '+last+' were not granted.');
+                                    self.errorHandle(res, null, 'invalid_scope', 'Required scopes '+errors.join(', ')+' and '+last+' where not granted.');
                                 } else if(errors.length > 0) {
                                     self.errorHandle(res, null, 'invalid_scope', 'Required scope '+errors.pop()+' not granted.');
                                 } else {
@@ -1170,6 +1212,49 @@ OpenIDConnect.prototype.removetokens = function() {
             ];
 };
 
+/**
+ * proxyKey
+ *
+ * returns a function to be placed as middleware in connect/express routing methods. For example:
+ *
+ * app.get('/key', oidc.proxyKey(),  sendKey, errorHandler);
+ *
+ * This get the public key associated with the IdP Proxy RP
+ *
+ *
+ */
+
+OpenIDConnect.prototype.proxyKey = function() {
+    var self = this;
+
+    return [self.use({policies: {loggedIn: false}, models: 'user'}),
+            function(req, res, next) {
+                validateUser(req, /*next:*/function(error,user) {
+                    if(!error && !user) {
+                        error = new Error('User not validated');
+                    }
+                    if(!error) {
+                        if(user.id) {
+                            req.session.user = user.id;
+                        } else {
+                            delete req.session.user;
+                        }
+                        if(user.sub) {
+                            if(typeof user.sub ==='function') {
+                                req.session.sub = user.sub();
+                            } else {
+                                req.session.sub = user.sub;
+                            }
+                        } else {
+                            delete req.session.sub;
+                        }
+                        return next();
+                    } else {
+                        return next(error);
+                    }
+                });
+    }];
+};
 exports.oidc = function(options) {
     return new OpenIDConnect(options);
 };
